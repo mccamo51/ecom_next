ed7381ee1519fd3038283aea0e4bfe71aeaedc03
Bismark <mccamo51@gmail.com>
2022-04-22 05:02:17 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less');
 
-module.exports = nextConfig
+module.exports = withAntdLess({
+  // optional
+  modifyVars: { '@primary-color': '#04f' },
+  // optional
+  // lessVarsFilePath: './src/styles/variables.less', // if you will keep variables in a separate file
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+
+  // ONLY for Next.js 10, if you use Next.js 11, delete this block
+  future: {
+    webpack5: true,
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import 'antd/dist/antd.css'
+// import 'tailwindcss/tailwind.css' // only use this if no components extracted for tailwind
+import './../globals.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
